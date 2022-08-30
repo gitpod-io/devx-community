5f8b459c513c18618d57b721444c56b0a47656b8
Julian <julian@gitpod.io>
2022-08-30 09:04:37 +0000

    feat: gitpodify project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { execSync } from 'node:child_process';
+
+let extendedViteDevServerOptions;
+
+try {
+	const gitpodPortUrl = execSync(`gp url ${5173}`).toString().trim();
+
+	extendedViteDevServerOptions = {
+		hmr: {
+			protocol: 'wss',
+			host: new URL(gitpodPortUrl).hostname,
+			clientPort: 443
+		}
+	};
+} catch {
+	extendedViteDevServerOptions = {};
+}
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		...extendedViteDevServerOptions
+	}
 };
 
 export default config;
