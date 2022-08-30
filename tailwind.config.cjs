041cc163f5a640575547f34fc8770e0ddeafc1d0
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-08-30 14:33:44 +0000

    feat: init design system (#8)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,55 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
+	darkMode: 'class',
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: 'var(--color-primary)',
+				headline: 'var(--color-headline)',
+				paragraph: 'var(--color-paragraph)',
+				brown: 'var(--color-brown)',
+				background: 'var(--color-background)',
+				boxes: 'var(--color-boxes)'
+			},
+			backgroundImage: {
+				primary: 'var(--gradient-primary)',
+				stroke: 'var(--gradient-stroke)'
+			},
+			spacing: {
+				'xx-large': '160px',
+				'x-large': '72px',
+				large: '64px',
+				medium: '56px',
+				small: '48px',
+				'x-small': '32px'
+			},
+			fontFamily: {
+				sans: ['Manrope', ...defaultTheme.fontFamily.sans]
+			},
+			fontSize: {
+				h2: [
+					'48px',
+					{
+						lineHeight: '54px'
+					}
+				],
+				h3: [
+					'28px',
+					{
+						lineHeight: '38.25px'
+					}
+				],
+				inter: [
+					'20px',
+					{
+						lineHeight: '26px'
+					}
+				],
+				body: ['18px', { lineHeight: '27px' }]
+			}
+		}
 	},
 
 	plugins: []
