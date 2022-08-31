3d9ac7782367ede8fc8b9e509dd62add0b0141de
nisarhassan12 <syednisarhassan12@gmail.com>
2022-08-31 09:43:00 +0500

    add developer experience and community sections.

---
+++ src/lib/components/dots.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let height = 18;
+	export let width = 18;
+	let clazz = '';
+	export { clazz as class };
+</script>
+
+<div
+	class="mx-auto mt-x-small grid grid-cols-2 gap-1 {clazz}"
+	style="max-width: {2 * width + 4}px;"
+>
+	{#each Array(4) as _}
+		<div class="rounded-full bg-primary" style="height: {height}px; width: {width}px" />
+	{/each}
+</div>
--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,10 +1,10 @@
-<header>
+<header class="bottom-lined">
 	<div class="mx-auto grid max-w-[310px] grid-cols-2 gap-4">
 		{#each Array(4) as _}
 			<div class="h-36 w-36 rounded-full bg-primary" />
 		{/each}
 	</div>
-	<h1 class="h3 mx-auto mt-x-large max-w-[300px] text-center">
+	<h1 class="h3 mx-auto mt-x-large max-w-[320px] text-center">
 		Bring back joy and speed to our workflows
 	</h1>
 </header>
