d2f699ef0a75f4543c679af07ec406ae2360821f
Julian <julian@gitpod.io>
2022-08-31 09:06:24 +0000

    ci: deploy netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,9 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			split: true
+		}),
 		files: {
 			lib: 'src/lib'
 		}
