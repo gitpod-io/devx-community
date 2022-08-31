16d3e9f2f3c7928a4869adae408e25177aff40fd
nisarhassan12 <syednisarhassan12@gmail.com>
2022-08-31 07:27:30 +0500

    initial draft of the her area.

---
+++ src/lib/components/hero.svelte
@@ -0,0 +1,13 @@
+<header>
+	<div class="mx-auto grid max-w-[310px] grid-cols-2 gap-4">
+		{#each Array(4) as _, i}
+			<div class="h-36 w-36 rounded-full bg-primary" />
+		{/each}
+	</div>
+	<h1 class="h3 mx-auto mt-x-large max-w-[300px] text-center">
+		Bring back joy and speed to our workflows
+	</h1>
+</header>
+
+<style lang="postcss">
+</style>
