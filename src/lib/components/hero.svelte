203c43e19eac4ca3504f45aaba17f455304b3256
nisarhassan12 <syednisarhassan12@gmail.com>
2022-08-31 15:36:33 +0500

    refactor the dot component.

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,13 +1,14 @@
+<script>
+	import Dot from './dot.svelte';
+</script>
+
 <header class="bottom-lined">
 	<div class="mx-auto grid max-w-[310px] grid-cols-2 gap-4">
 		{#each Array(4) as _}
-			<div class="h-36 w-36 rounded-full bg-primary" />
+			<Dot class="h-36 w-36" />
 		{/each}
 	</div>
 	<h1 class="h3 mx-auto mt-x-large max-w-[360px] text-center">
 		Bring back joy and speed to our workflows
 	</h1>
 </header>
-
-<style lang="postcss">
-</style>
