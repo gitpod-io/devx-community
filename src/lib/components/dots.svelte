02ae19b276ac7fb661bc5776e57e07255cd81e6d
Julian <julian@gitpod.io>
2022-08-31 10:33:24 +0000

    revert: dots

--- src/lib/components/dots.svelte
+++ src/lib/components/dots.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-	import Dot from './library/dot.svelte';
-	const size = 18;
+	export let height = 18;
+	export let width = 18;
 	let clazz = '';
 	export { clazz as class };
 </script>
 
-<div class="mx-auto mt-x-small grid grid-cols-2 gap-1 {clazz}" style="max-width: {2 * size + 4}px;">
+<div
+	class="mx-auto mt-x-small grid grid-cols-2 gap-1 {clazz}"
+	style="max-width: {2 * width + 4}px;"
+>
 	{#each Array(4) as _}
-		<Dot {size} />
+		<div class="rounded-full bg-primary" style="height: {height}px; width: {width}px" />
 	{/each}
 </div>
--- src/lib/components/library/dot.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	export let size = 18;
-</script>
-
-<div class="rounded-full bg-primary" style="height: {size}px; width: {size}px" />
