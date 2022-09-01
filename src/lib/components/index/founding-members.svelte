4293e69f287c5eb4c495f3cf46c493e28bfa04c9
Julian <julian@gitpod.io>
2022-09-01 10:58:58 +0000

    chore: prepare founding members

--- src/lib/components/index/founding-members.svelte
+++ src/lib/components/index/founding-members.svelte
@@ -1,11 +1,16 @@
 <script>
 	import Section from '../library/section.svelte';
+	import PortraitCard from '../library/portrait-card.svelte';
+	import { foundingMembers } from '$lib/contents/index/founding-members';
 </script>
 
-<Section title="Founding members">
-	<div class="flex flex-wrap justify-center gap-x-small">
-		{#each Array(4) as _}
-			<div class="h-56 w-56 rounded-2xl border border-solid border-highlight" />
-		{/each}
-	</div>
-</Section>
+<div class="mx-auto max-w-5xl">
+	<Section titleClassNames="pb-x-small" title="Founding members">
+		<p class="pb-large text-center">DevX Community was built by a community of industry experts</p>
+		<div class="mt-x-large grid grid-cols-1 gap-y-large gap-x-small sm:grid-cols-2 xl:grid-cols-4">
+			{#each foundingMembers as item}
+				<PortraitCard portrait={item} />
+			{/each}
+		</div>
+	</Section>
+</div>
--- src/lib/components/library/portrait-card.svelte
+++ src/lib/components/library/portrait-card.svelte
@@ -1,23 +1,29 @@
 <script lang="ts">
+	import type { PortraitType } from '$lib/types/portrait.type';
+
 	import Github from '../svgs/github.svelte';
 	import Twitter from '../svgs/twitter.svelte';
 
-	export let imgsrc: string;
-	export let name: string;
-	export let company: string;
-	export let githubLink = '';
-	export let twitterLink = '';
+	export let portrait: PortraitType;
 </script>
 
-<div class="flex flex-col items-center justify-center">
-	<div class="relative inline-block">
-		<img class="rounded-2xl grayscale" width="170" height="170" alt="test" src={imgsrc} />
-		<div class="absolute -top-5 -left-8 -z-10 h-16 w-16 rounded-full bg-primary" />
-	</div>
-	<div class="mt-2 font-bold text-important">{name}</div>
-	<div class="mt-2">{company}</div>
-	<div class="mt-4 flex justify-center gap-[0.75rem]">
-		<a target="_blank" href={githubLink}> <Github /></a>
-		<a target="_blank" href={twitterLink}><Twitter /></a>
+<div>
+	<div class="flex flex-col items-center justify-center">
+		<div class="relative inline-block">
+			<img
+				class="rounded-2xl grayscale"
+				width="170"
+				height="170"
+				alt="photo of {portrait.name}"
+				src={portrait.imgsrc}
+			/>
+			<div class="absolute -top-5 -left-8 -z-10 h-16 w-16 rounded-full bg-primary" />
+		</div>
+		<div class="mt-2 font-bold text-important">{portrait.name}</div>
+		<div class="mt-2">{portrait.company}</div>
+		<div class="mt-4 flex justify-center gap-[0.75rem]">
+			<a target="_blank" href={portrait.githubLink}> <Github /></a>
+			<a target="_blank" href={portrait.twitterLink}><Twitter /></a>
+		</div>
 	</div>
 </div>
