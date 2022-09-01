7f3592d7b0aa87bf4fc147c8b35532a2cc978799
nisarhassan12 <syednisarhassan12@gmail.com>
2022-09-01 21:05:22 +0500

    refactor.

--- src/lib/components/developer-experience.svelte
+++ src/lib/components/developer-experience.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import AnimatedText from './library/animated-text.svelte';
+	import Container from './layout/container.svelte';
 	import Dot from './library/dot.svelte';
 	import FullBleed from './library/full-bleed.svelte';
-	import Section from './section.svelte';
+	import Section from './library/section.svelte';
 </script>
 
 <Section
@@ -12,17 +13,19 @@
 	class="relative"
 >
 	<FullBleed class="mt-x-large w-screen bg-box py-x-large">
-		<div class="mx-auto max-w-xl">
-			<div class="mt-x-small grid grid-cols-2 gap-1" style="max-width: {2 * 18 + 4}px;">
-				{#each Array(4) as _}
-					<Dot class="h-[18px] w-[18px]" />
-				{/each}
+		<Container>
+			<div class="mx-auto max-w-xl">
+				<div class="mt-x-small grid grid-cols-2 gap-1" style="max-width: {2 * 18 + 4}px;">
+					{#each Array(4) as _}
+						<Dot class="h-[18px] w-[18px]" />
+					{/each}
+				</div>
+				<AnimatedText
+					class="text-large my-x-small text-sub"
+					text="[..] developers are forced to borrow time from writing code and redirect it towards managing the issues associated with highly complex, multi-factor developer toolchains held together in places by duct tape and baling wire.”"
+				/>
+				<p>The Developer Experience Gap” by Stephen O'Grady, Redmonk</p>
 			</div>
-			<AnimatedText
-				class="text-large my-x-small text-sub"
-				text="[..] developers are forced to borrow time from writing code and redirect it towards managing the issues associated with highly complex, multi-factor developer toolchains held together in places by duct tape and baling wire.”"
-			/>
-			<p>The Developer Experience Gap” by Stephen O'Grady, Redmonk</p>
-		</div>
+		</Container>
 	</FullBleed>
 </Section>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
 	import Nav from '$lib/components/layout/nav.svelte';
 	import Footer from '$lib/components/layout/footer.svelte';
+	import Container from '$lib/components/layout/container.svelte';
 	import '../app.css';
 </script>
 
 <div class="flex min-h-screen flex-col">
 	<Nav />
 
-	<main
-		class="mx-auto max-w-7xl px-4 pb-x-large pt-xx-large sm:px-x-small md:px-medium lg:px-large xl:px-0"
-	>
+	<Container element="main" class="pt-xx-large">
 		<slot />
-	</main>
+	</Container>
 	<Footer />
 </div>
