8ab42bf4f5423e3d1ec28e4b0cb970e6f4849a1e
Julian <julian@gitpod.io>
2022-09-02 12:47:21 +0000

    feat: add image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,17 @@
 	import WhatIsThisCommunity from '$lib/components/index/what-is-this-community.svelte';
 	import DeveloperExperience from '$lib/components/developer-experience.svelte';
 	import JoinNow from '$lib/components/index/fragments/join-now.svelte';
+
+	import OpenGraph from '$lib/components/library/open-graph.svelte';
 </script>
+
+<OpenGraph
+	data={{
+		title: 'A dedicated community space for developer experience.',
+		description:
+			"A community built to bring together folks dedicated to making developers' daily experience (devX) efficient, sustainable and joyful."
+	}}
+/>
 
 <Hero />
 
