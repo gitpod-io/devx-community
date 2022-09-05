d7ffe752a14cfb36353096412b63505b851f6bde
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-09-05 13:24:27 +0000

    feat: privacy policy (#61)

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,1 +1,335 @@
-<h1 class="h2 mb-x-large text-center">Privacy Policy</h1>
+<h1 class="h2 mb-small text-center">Privacy Policy</h1>
+<div class="mb-x-large text-center text-xs">Last updated September 5, 2022</div>
+<div class="mx-auto max-w-xl">
+	<p class="mb-small">
+		As this is a static website, we won’t be collecting any personal identifiable information about
+		you in the context of the European General Data Protection Regulation (GDPR), unless you choose
+		to contact us via email addresses posted on this website. Furthermore, this website is not using
+		any advertisements, analytics, user tracking or cookies. The controller is Gitpod GmbH, Am
+		Germaniahafen 1, 24143 Kiel/Germany, registered with the commercial register of the local court
+		(Amtsgericht) Kiel under HRB 22228 ("we/us/our" or "Gitpod"). We offer services to our users and
+		visitors (the "User/you/your") on our website https://developerexperience.us ("Website"). For
+		any questions about data protection you may contact us via <a href="mailto:data@gitpod.io"
+			>data@gitpod.io</a
+		>. If you would like to understand more about our privacy practices concerning Gitpod products
+		and services, please refer to
+		<a href="https://www.gitpod.io/privacy">https://www.gitpod.io/privacy</a>
+	</p>
+	<h2 class="text-h3">In Short</h2>
+	<ul class="list-disc space-y-small">
+		<li>
+			<h3 class="font-bold text-important">Controller</h3>
+			<p>
+				Gitpod GmbH, am Germaniahafen 1, 24143 Kiel/Germany, registered with the commercial register
+				of the local court (Amtsgericht) Kiel under HRB 22228, Email: <a
+					href="mailto:contact@gitpod.io">contact@gitpod.io</a
+				>. We have appointed a data protection officer who may be contacted via
+				<a href="mailto:data@gitpod.io">data@gitpod.io</a>.
+			</p>
+		</li>
+		<li>
+			<h3 class="font-bold text-important">
+				Purpose and Legal Basis of Processing Data; Provision and Recipients of Data
+			</h3>
+			<p>
+				When contacting us via email, the User’s details (name and email) are stored for the purpose
+				of processing the enquiry and, if applicable, follow-up questions based on your consent
+				(under Art. 6 (1) a. GDPR) or fulfilling your request on a contractual basis (under Art. 6
+				(1) b. GDPR)
+			</p>
+		</li>
+		<li>
+			<h3 class="font-bold text-important">Your Rights</h3>
+			<p>
+				You have the right to withdraw your consent relating to the use of data any time with effect
+				for the future when such data processing is based on your consent. You are entitled to
+				access the data stored by us and are also entitled to amend or rectify your data if such
+				data are incorrect. You have the right to object to the processing of your personal data,
+				for example if your personal data are processed for direct marketing purposes. You are
+				entitled to request the erasure of your data. You are entitled to receive information about
+				the stored data (in a structured, current and machine-readable format) at any time and to
+				request the correction or deletion of the data in case of incorrect data storage. You have
+				also the right to lodge a complaint with a supervisory authority of your choice. An overview
+				of the European National Data Protection Authorities may be found <a
+					href="http://ec.europa.eu/newsroom/article29/item-detail.cfm?item_id=612080">here</a
+				>.
+			</p>
+		</li>
+		<li>
+			<h3 class="font-bold text-important">Period for Storing Data; Deletion</h3>
+			<p>
+				The data are deleted if such data are no longer necessary for the purpose of processing,
+				unless required by law to be retained.
+			</p>
+		</li>
+		<li>
+			<h3 class="font-bold text-important">Data Security</h3>
+			<p>
+				We have implemented measures to ensure data and IT security. The Website is operated through
+				a safe SSL-connection. If an SSL-connection is activated third parties are prevented from
+				reading any data that are transferred by you to us.
+			</p>
+		</li>
+	</ul>
+	<h2 class="text-h3">More Detailed Information</h2>
+	<ul class="list-decimal space-y-2 pb-small">
+		<li>
+			<a href="https://www.gitpod.io/privacy#details-I">What are Personal Data?</a>
+		</li>
+		<li>
+			<a href="https://www.gitpod.io/privacy#details-III"
+				>How are my Data processed when visiting the Website and contacting us?</a
+			>
+		</li>
+		<li>
+			<a href="https://www.gitpod.io/privacy#details-IV"
+				>What Third Party Services, Cookies, Analytics and Social Plugins does the Website use?</a
+			>
+		</li>
+		<li>
+			<a href="https://www.gitpod.io/privacy#details-V"
+				>How are my data processed when using the Gitpod Service (with User Account)?</a
+			>
+		</li>
+		<li>
+			<a href="https://www.gitpod.io/privacy#details-VI"
+				>How are Data processed when visiting our Social Media pages?</a
+			>
+		</li>
+		<li>
+			<a href="https://www.gitpod.io/privacy#details-VII"
+				>Are my Data transferred to Third Parties? Does automated decision making including
+				profiling take place?</a
+			>
+		</li>
+		<li>
+			<a href="https://www.gitpod.io/privacy#details-VIII"
+				>Are my Data transferred outside the EU?</a
+			>
+		</li>
+		<li>
+			<a href="https://www.gitpod.io/privacy#details-IX">Your Rights</a>
+		</li>
+		<li>
+			<a href="https://www.gitpod.io/privacy#details-X"
+				>Duration of Storing your Data; Data Security; Access and Changes to this Privacy Policy;
+				Contact Details</a
+			>
+		</li>
+	</ul>
+	<ul class="list-decimal">
+		<li class="marker:font-bold">
+			<h3 class="font-bold text-important">What is Personal Data?</h3>
+			<p>
+				We have implemented measures to ensure data and IT security. The Website is operated through
+				a safe SSL-connection. If an SSL-connection is activated third parties are prevented from
+				reading any data that are transferred by you to us.
+			</p>
+		</li>
+		<li class="marker:font-bold">
+			<h3 class="font-bold text-important">Applicable Laws to our processing of Personal Data</h3>
+			<p>
+				We have implemented measures to ensure data and IT security. The Website is operated through
+				a safe SSL-connection. If an SSL-connection is activated third parties are prevented from
+				reading any data that are transferred by you to us.
+			</p>
+		</li>
+		<li class="marker:font-bold">
+			<h3 class="font-bold text-important">
+				How is my Data processed when visiting the Website and contacting us?
+			</h3>
+			<br />
+			<h4 class="font-bold text-important">Visiting the Website</h4>
+			<br />
+			<br />
+			<p>This website is not collecting any personal information about you.</p>
+			<h4 class="font-bold text-important">Contacting us</h4>
+			<br />
+			<br />
+			<p>
+				When contacting us via email, the User’s details (name and email) are stored for the purpose
+				of processing the enquiry and, if applicable, follow-up questions based on your consent
+				(under Art. 6 (1) a. GDPR) or fulfilling your request on a contractual basis (under Art. 6
+				(1) b. GDPR). Use of Front: the mail provider “Front” by Frontapp, Inc., 1455 Market St Fl
+				19, San Francisco, California, 94102, United States receives and processes on our behalf the
+				data necessary to manage client inquiries and emails. For more information refer to
+				https://front.com/privacy-policy.
+			</p>
+		</li>
+		<li class="marker:font-bold">
+			<h3 class="font-bold text-important">
+				What Third Party Services, Cookies, Analytics and Social Plugins does the Website use?
+			</h3>
+			<br />
+			<h4 class="font-bold text-important">Cookies</h4>
+			<p>
+				This website is not using any cookies, third party services, analytics or social plugins.
+			</p>
+		</li>
+		<li class="marker:font-bold">
+			<h3 class="font-bold text-important">
+				Are my Data transferred to Third Parties? Does Profiling or automated decision making take
+				place?
+			</h3>
+			<br />
+			<h4 class="font-bold text-important">Transfer of Data to Third Parties</h4>
+			<p>
+				We will transfer your personal data to a third party only (i) within the scope of legal
+				provisions, i.e. if we are obliged to transfer the data due to a government or court order,
+				or, (ii) if applicable, legal provisions authorize the transfer, e.g. if the transfer is
+				required to pursue our legitimate interests or to fulfil a contract, for example in case of
+				mergers and acquisitions, (iii) or if you give your explicit consent.
+				<br /><br />
+				For more information please refer to this privacy policy or contact us via
+				<a href="mailto:data@gitpod.io">data@gitpod.io</a>.
+			</p>
+			<h4 class="font-bold text-important">Automated Decision Making including Profiling</h4>
+			<p>
+				In general we do not process any personal data via automated decision making including
+				profiling via the Website or Service. However, such profiling or automated decision making
+				may happen by third party providers through the Website or Service. We will inform you about
+				such fact if possible.
+				<br /> <br />
+				Profiling means any automated processing of personal data to evaluate certain personal aspects
+				relating to a natural person, in particular to analyse or predict aspects relating to the performance
+				of work, economic situation, health, personal preferences, interests, reliability, behaviour,
+				location or relocation of that natural person. Examples of such profiling include the analysis
+				of data (e.g. based on statistical methods) with the aim of displaying personalized advertising
+				to the user or (for example) giving shopping tips. The data subject shall not be subject to a
+				decision based exclusively on automated processing, including profiling, which has legal effect
+				against them or significantly affects them in a similar manner.
+			</p>
+		</li>
+		<li class="marker:font-bold">
+			<h3 class="font-bold text-important">Is my Data transferred outside the EU?</h3>
+			<p>
+				When contacting us via email, your data may be transferred to countries outside the EU by
+				the third party services referred to in <a href="https://www.gitpod.io/privacy#details-III"
+					>Section III</a
+				>.
+			</p>
+		</li>
+		<li class="marker:font-bold">
+			<h4 class="font-bold text-important">Your Rights</h4>
+			As a data subject you have the right:
+			<ul class="ml-4 list-disc md:ml-8">
+				<li>
+					(where we are relying on your consent) to withdraw your consent to us at any time. As a
+					result, we are no longer allowed to continue the processing of data based on this consent
+					in the future;
+				</li>
+				<li>
+					to object to the processing of your personal data, if your personal data are processed on
+					the basis of legitimate interests pursuant to Art. 6 (1) f. GDPR insofar as there are
+					reasons for this arising from your particular situation;
+				</li>
+				<li>to obtain from us access to your personal data;</li>
+				<li>
+					to obtain from us without undue delay the rectification of inaccurate personal data
+					concerning you;
+				</li>
+				<li>
+					to obtain the erasure of your personal data stored with us, unless the processing is
+					necessary to exercise the right to free expression of opinion and information, to fulfil a
+					legal obligation, for reasons of public interest or to assert, exercise or defend legal
+					claims;
+				</li>
+				<li>
+					to request the restriction of the processing of your personal data, if e.g. the accuracy
+					of the data is disputed by you, the processing is unlawful, but you refuse its deletion
+					and we no longer need the data, but you need it to assert, exercise or defend legal claims
+					or you have filed an objection against the processing; and
+				</li>
+				<li>
+					to receive your personal data, which you have provided to us, in a structured, current and
+					machine-readable format or to request the transmission to another controller.
+				</li>
+			</ul>
+			<br />
+			<p>
+				If you wish to make use of your rights mentioned above please send an email to <a
+					href="mailto:data@gitpod.io">data@gitpod.io</a
+				>.
+				<br /><br />
+				If you obtain access to your personal data you may, in particular, request access to the following
+				information: the purposes of the processing, the categories of personal data concerned, the recipients
+				or categories of recipients to whom the personal data have been or will be disclosed.
+				<br /><br />
+				You have the right to lodge a complaint vis-à-vis a supervisory authority of your choice. For
+				example for Berlin/Germany:
+				<a rel="noopener" target="_blank" href="https://www.datenschutz-berlin.de/kontakt.html"
+					>https://www.datenschutz-berlin.de/kontakt.html</a
+				>. An overview of the European National Data Protection Authorities may be found here:
+				<a
+					rel="noopener"
+					href="http://ec.europa.eu/newsroom/article29/item-detail.cfm?item_id=612080"
+					target="_blank"
+				>
+					http://ec.europa.eu/newsroom/article29/item-detail.cfm?item_id=612080</a
+				>
+			</p>
+		</li>
+		<li class="marker:font-bold">
+			<h3 class="font-bold text-important">
+				Duration of Storing your Data; Data Security; Access and Changes to this Privacy Policy;
+				Contact Details
+			</h3>
+			<br />
+			<h4 class="font-bold text-important">Duration of Storing your Data</h4>
+			<p>
+				As a rule, we only store your personal data for as long as it is necessary for the execution
+				of the contract or the respective purpose and limit the storage period to an absolutely
+				necessary minimum.
+				<br /> <br />
+				Criteria for the storage period include whether the data are still up-to-date, whether the contractual
+				relationship with us still exists, whether an inquiry has already been processed, whether a process
+				has been completed or not, and whether any legal retention periods for the personal data concerned
+				are relevant or not.
+			</p>
+			<h4 class="font-bold text-important">Data Security</h4>
+			<p>
+				We have installed technical and organisational measures in order to safeguard our Website
+				and/or Service against loss, destruction, access, changes or the distribution of your data
+				by unauthorised persons.
+				<br /> <br />
+				The Website and Service is operated through a safe SSL-connection. If an SSL-connection is activated
+				third parties are prevented from reading any data that is transferred by you to us.
+				<br /> <br />
+				We store Personal Information in encrypted format, leveraging state-of-the-art encryption algorithms.
+				<br /> <br />
+				We also maintain an Information Security & Privacy program with established policies and procedures
+				that govern the confidentiality, integrity and availability of data.
+			</p>
+			<h4 class="font-bold text-important">Access and Changes to this Privacy Policy</h4>
+			<p>
+				This privacy policy is accessible via <a href="/privacy">our Website</a> and may be
+				downloaded and printed anytime.
+				<br /> <br />
+				We reserve the right to change this privacy policy at any time, taking into account currently
+				applicable data protection provisions. In case of any changes, an update will be posted to our
+				website showing the new version of the policy and the date of last review.
+			</p>
+			<h4 class="font-bold text-important">Contact</h4>
+			<p>
+				For any inquiries and additional questions about processing personal data please contact <a
+					href="mailto:data@gitpod.io">data@gitpod.io</a
+				>
+				<br /> <br />
+				Further details may be found <a href="/imprint">here</a>.
+			</p>
+		</li>
+	</ul>
+</div>
+
+<style lang="postcss">
+	ul {
+		@apply list-inside sm:list-outside;
+	}
+	p {
+		@apply mb-medium;
+	}
+
+	h2 {
+		@apply mb-small;
+	}
+</style>
