de2ddf0cd27f9194997535fee17aa66d4acad96e
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2022-09-05 20:18:20 +0500

    adjust the spacing in the hero. (#67)

--- src/lib/components/index/hero.svelte
+++ src/lib/components/index/hero.svelte
@@ -38,7 +38,7 @@
 		<Dot class="h-24 w-24 lg:h-36 lg:w-36" />
 		<Dot class="h-24 w-24 lg:h-36 lg:w-36" />
 	</div>
-	<h1 class="h3 mx-auto mt-x-large max-w-[510px] text-center">
+	<h1 class="h3 mx-auto mt-small max-w-[510px] text-center">
 		A dedicated space<br />for developer experience
 	</h1>
 	<AnimatedLine disableObserver={true} visible={isLineVisible} />
