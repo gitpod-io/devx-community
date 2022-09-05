9c034277b166b0e48a7c890b0f46fdcc83d9238a
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-09-05 15:05:11 +0000

    feat: add images (#63)

--- src/lib/contents/index/founding-members.ts
+++ src/lib/contents/index/founding-members.ts
@@ -2,27 +2,26 @@ import type { Portrait } from '$lib/types/portrait.type';
 
 export const foundingMembers: Portrait[] = [
 	{
-		imgsrc: 'https://amulett.at/wp-content/uploads/2018/10/portrait-square-05.jpg',
+		imgsrc: '/images/portraits/cirpo.jpeg',
 		name: 'Cirpo',
 		company: '@Spotify',
 		role: 'Engineering Manager - Core Infra',
-		githubLink: '',
+		linkedinLink: 'https://www.linkedin.com/in/cirpo/',
 		twitterLink: 'https://twitter.com/cirpo'
 	},
 	{
-		imgsrc: 'https://amulett.at/wp-content/uploads/2018/10/portrait-square-05.jpg',
+		imgsrc: '/images/portraits/elvis_kahoro.jpeg',
 		name: 'Elvis',
 		company: '@Warp',
 		role: 'DevX Engineer',
-		githubLink: '',
-		twitterLink: ''
+		linkedinLink: 'https://www.linkedin.com/in/elviskahoro/'
 	},
 	{
-		imgsrc: 'https://amulett.at/wp-content/uploads/2018/10/portrait-square-05.jpg',
+		imgsrc: 'images/portraits/abi_noda.png',
 		name: 'Abi Noda',
 		company: '@DX',
 		role: 'CEO',
-		githubLink: '',
+		linkedinLink: 'https://www.linkedin.com/in/abinoda/',
 		twitterLink: 'https://twitter.com/abinoda'
 	},
 	{
@@ -30,23 +29,23 @@ export const foundingMembers: Portrait[] = [
 		name: 'Brook Perry',
 		company: '@DX',
 		role: 'Marketing',
-		githubLink: '',
+		linkedinLink: 'https://www.linkedin.com/in/brookperry/',
 		twitterLink: 'https://twitter.com/brookprry'
 	},
 	{
-		imgsrc: 'https://amulett.at/wp-content/uploads/2018/10/portrait-square-05.jpg',
+		imgsrc: '/images/portraits/josh_maxwell.jpeg',
 		name: 'Josh Maxwell',
 		company: '@Qualtrics',
-		githubLink: '',
+		linkedinLink: 'https://www.linkedin.com/in/cctechwiz/',
 		role: 'Software Engineer',
 		twitterLink: 'https://twitter.com/cctechwiz'
 	},
 	{
-		imgsrc: 'https://amulett.at/wp-content/uploads/2018/10/portrait-square-05.jpg',
+		imgsrc: '/images/portraits/phillip_carter.jpeg',
 		name: 'Phillip Carter',
 		company: '@Honeycomb',
 		role: 'Product & DX',
-		githubLink: '',
+		linkedinLink: 'https://www.linkedin.com/in/phillip-carter-4714a135/',
 		twitterLink: 'https://twitter.com/_cartermp'
 	},
 	{
@@ -54,15 +53,15 @@ export const foundingMembers: Portrait[] = [
 		name: 'Josep Jaume',
 		role: 'Engineering Director',
 		company: '@Factorial',
-		githubLink: '',
+		linkedinLink: 'https://www.linkedin.com/in/josep-jaume-rey-peroy-a9701622/',
 		twitterLink: 'https://twitter.com/josepjaume'
 	},
 	{
-		imgsrc: 'https://amulett.at/wp-content/uploads/2018/10/portrait-square-05.jpg',
+		imgsrc: '/images/portraits/geoffrey_teale.jpg',
 		name: 'Geoffrey Teale',
 		company: '@Upvest',
 		role: 'Head of Developer Experience',
-		githubLink: '',
+		linkedinLink: 'https://www.linkedin.com/in/geoffteale/',
 		twitterLink: 'https://twitter.com/tealeg'
 	}
 ];
