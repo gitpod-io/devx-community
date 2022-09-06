7156cc8e987e91a14a98bcb9018cd17906a0f3c4
Julian <julian@gitpod.io>
2022-09-06 09:34:54 +0000

    style: hover

--- src/lib/components/library/gradient-card.svelte
+++ src/lib/components/library/gradient-card.svelte
@@ -11,15 +11,26 @@
 	{target}
 	{href}
 	{rel}
-	class="gradient-border block h-full rounded-2xl border-[2.5px] border-double border-transparent px-3 py-6 no-underline transition-all delay-50 duration-200 hover:border-4 hover:shadow-hover-default {className}"
+	class="gradient-border block h-full rounded-2xl border-[2.5px] border-double border-transparent px-3 py-6 no-underline {className}"
 >
 	<slot />
 </a>
 
 <style lang="postcss">
 	.gradient-border {
-		background-image: linear-gradient(var(--bg), var(--bg)), var(--gradient-primary);
+		@apply relative z-10;
+		background: linear-gradient(var(--bg), var(--bg)), var(--gradient-primary);
 		background-origin: border-box;
 		background-clip: padding-box, border-box;
+	}
+	.gradient-border::before {
+		@apply absolute top-0 left-0 -z-10 h-full w-full rounded-2xl opacity-0 transition-all delay-50 duration-200;
+		content: '';
+		background: linear-gradient(var(--box), var(--box)), var(--gradient-primary);
+		background-origin: border-box;
+		background-clip: padding-box, border-box;
+	}
+	.gradient-border:hover::before {
+		@apply opacity-100;
 	}
 </style>
--- src/lib/components/library/gradient-link-button.svelte
+++ src/lib/components/library/gradient-link-button.svelte
@@ -8,15 +8,26 @@
 	{target}
 	{href}
 	{rel}
-	class="gradient-border tracking[0.2em] h5 inline-block rounded-[40px] border-[2.5px] border-double border-transparent px-6 py-4  font-bold text-explicit-white-light no-underline transition-all delay-50 duration-200 hover:border-[2.5px] hover:shadow-hover-default sm:px-8"
+	class="gradient-border tracking[0.2em] h5 inline-block rounded-[40px] border-[2.5px] border-double border-transparent px-6 py-4 font-bold text-explicit-white-light no-underline sm:px-8"
 >
 	<slot />
 </a>
 
 <style lang="postcss">
 	.gradient-border {
+		@apply relative z-10;
 		background-image: linear-gradient(var(--black), var(--black)), var(--gradient-primary);
 		background-origin: border-box;
 		background-clip: padding-box, border-box;
+	}
+	.gradient-border::before {
+		@apply absolute top-0 left-0 -z-10 h-full w-full rounded-[40px] opacity-0 transition-all delay-50 duration-200;
+		content: '';
+		background-image: linear-gradient(var(--dark-black), var(--dark-black)), var(--gradient-primary);
+		background-origin: border-box;
+		background-clip: padding-box, border-box;
+	}
+	.gradient-border:hover::before {
+		@apply opacity-100;
 	}
 </style>
