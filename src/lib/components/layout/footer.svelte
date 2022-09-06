63a933b7e0cd5fa43bef747d66f80b47a2fee51b
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-09-06 08:37:02 +0000

    style: footer alignment (#79)

--- src/lib/components/layout/footer.svelte
+++ src/lib/components/layout/footer.svelte
@@ -8,10 +8,10 @@
 	<footer
 		class="m-auto px-4 py-xx-small text-base text-body md:max-w-6xl md:px-x-small md:py-small md:text-main"
 	>
-		<div class="flex flex-col md:flex-row md:justify-around">
+		<div class="grid grid-cols-1 md:grid-cols-3">
 			<FooterDots class="mb-4 flex md:hidden" />
-			<div class="flex flex-col md:gap-x-small">
-				<p class="max-w-[220px]">DevX - a space dedicated to developer experience.</p>
+			<div class="flex w-full max-w-[220px] flex-col md:gap-x-small">
+				<p class="">DevX - a space dedicated to developer experience.</p>
 				<p class="mb-4 md:mb-0 md:!mt-0">
 					An initative organised by Gitpod.
 					<br />
@@ -20,7 +20,7 @@
 				<Gitpod class="w-16 md:w-24" />
 			</div>
 			<FooterDots class="hidden items-center md:flex" />
-			<div class="mt-medium flex w-full max-w-[200px] flex-col gap-x-small md:mt-0 md:items-end">
+			<div class="mt-medium flex w-full flex-col gap-x-small md:mt-0 md:items-end">
 				<a target="_blank" href="https://twitter.com/devxconf"><Twitter /></a>
 			</div>
 		</div>
