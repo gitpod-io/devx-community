bfa014a78c2fa4c0d487dee9fbb9d38d3d98de97
nisarhassan12 <syednisarhassan12@gmail.com>
2022-09-07 16:43:10 +0500

    add the big dots.

--- src/lib/components/library/content-section.svelte
+++ src/lib/components/library/content-section.svelte
@@ -4,9 +4,11 @@
 	export let icon: any;
 	export let title: string;
 	export let annotation: string;
+	let className = '';
+	export { className as class };
 </script>
 
-<section {id} class="mx-auto max-w-xl">
+<section {id} class="mx-auto max-w-xl {className}">
 	<div class="flex items-center gap-4">
 		<h3 class="h3 inline">{title}</h3>
 		<svelte:component this={icon} />
