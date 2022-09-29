f344a4e8dc1202128b10209df94eb61d8d0feebc
Julian <julian@gitpod.io>
2022-09-29 13:22:37 +0000

    fix: previous

--- src/routes/api/env/+server.ts
+++ src/routes/api/env/+server.ts
@@ -1,6 +1,6 @@
 import { env } from '$env/dynamic/private';
 import { json } from '@sveltejs/kit';
 
-export const POST = async () => {
+export const GET = async () => {
 	return json({ context: env.CONTEXT });
 };
