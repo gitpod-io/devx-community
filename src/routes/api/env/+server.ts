29bd5f3e26583d21b6abbb70341471cb96487362
Julian <julian@gitpod.io>
2022-09-29 13:50:22 +0000

    chore: branch

--- src/routes/api/env/+server.ts
+++ src/routes/api/env/+server.ts
@@ -5,8 +5,7 @@ export const GET = async () => {
 	return json({
 		success: true,
 		context: env.CONTEXT,
-		secondContext: process.env.CONTEXT,
-		URL: env.URL,
-		secondURL: process.env.URL
+		branch: env.BRANCH,
+		URL: env.URL
 	});
 };
